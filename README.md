6ff632dff28384b9a2106b63a6f7ef59623be4d3
Dan Anthony <dananthony1@gmail.com>
2023-10-29 09:59:46 -0400

    Edit book_lover package files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DS5100-M09-HW-Booklover-Package
+
+Practice Package for uploading files to GitHub.
+
+Previous HW modules are uploaded and require some edits to make the booklover classes and methods functional.
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="Demo",
+    name="booklover",
     version='0.1',
-    description='A simple demonstration package',
-    url='http://github.com/dananthony1/DS5100-M09-Module-Exercise',
+    description='A simple practice example for uploading a package to GitHub',
+    url='https://github.com/dananthony1/DS5100-dja8tx/tree/main/lessons/M09/HW',
     author='D.J.Anthony',
     author_email='dananthony1',
     license='MIT',
-    packages=['demo'])
+    packages=['booklover'])
+)
